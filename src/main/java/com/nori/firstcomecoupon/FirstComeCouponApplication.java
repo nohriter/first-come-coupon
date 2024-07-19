0f2945b8c6b9e1391547098f30b6ac4834aba1a0
nohriter <nohriter@gmail.com>
2024-07-19 10:51:55 +0900

    :tada: Project 생성

---
+++ src/main/java/com/nori/firstcomecoupon/FirstComeCouponApplication.java
@@ -0,0 +1,13 @@
+package com.nori.firstcomecoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstComeCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstComeCouponApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nori/firstcomecoupon/FirstComeCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nori.firstcomecoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstComeCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
